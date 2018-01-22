a02115d7c235209e1ead876a2ccc51cc3b9bc688
joyloove <jocelyn.esquivel24@gmail.com>
2018-01-21 21:32:11 -0600

    Subiendo vistas y json

---
+++ README.md
@@ -0,0 +1,23 @@
+# ♡ F O O D M A P ♡
+Crea una web-app que a través de un input pueda filtrar los restaurantes que se encuentran cerca de ti (Tú decides la estructura que tendrán tus datos, puedes crear una lista de restaurantes en un arreglo, en un objeto, de manera individual. El diseño es totalmente libre).
+
+FLUJO DE LA APLICACIÓN.
+Vista splash con duración de 2 a 5 segundos que redirecciona a tu vista principal.
+
+![splash.jpg](assets/images/splash.jpg)
+
+En la vista principal se muestran todos los restaurantes "cerca de ti" junto con el input para filtrar tu elección (Los criterios de filtrado son decisión tuya, puede ser por tipo de comida, costos, orden alfabético, etc.).
+
+![2.jpg](assets/images/2.jpg)
+
+Ya que se hizo la elección del filtro deben mostrarse únicamente aquellos restaurantes que cumplan con la condición.
+
+![3.jpg](assets/images/3.jpg)
+
+Al seleccionar alguno de los restaurantes, deberá mostrarse la información de este a través de un modal.
+
+![5.jpg](assets/images/5.jpg)
+
+Una vez cerrado el modal debe volver a la vista principal.
+
+![6.jpg](assets/images/6.jpg)
---
+++ js/json.js
@@ -0,0 +1,81 @@
+var data = {
+  PERRONEGRO: {
+    name: 'Pizzas del Perro Negro',
+    food: ['pizzas', 'papas', 'pastas', 'ensaladas'],
+    address: 'Parque España 3, Col. Roma, 06700 Ciudad de México, CDMX',
+    price: '$180-$250',
+    image: '../assets/images/perro.jpg'
+  },
+
+  LABARRA: {
+    name: 'La Barra',
+    food: ['pizzas', 'empanadas', 'buffette', 'ensaladas'],
+    address: 'Medellin 125, Planta Alta. Col. Roma Norte, 06700 Ciudad de México, CDMX',
+    price: '$70-$200',
+    image: '../assets/images/barra.jpg'
+  },
+
+  LANONNAFINA: {
+    name: 'La Nonna Fina',
+    food: ['pizzas', 'ensaladas', 'carnes frías'],
+    address: ' Jalapa 217 A, Col. Roma, 06700 Ciudad de México, CDMX',
+    price: '$150-$300',
+    image: '../assets/images/nonna.jpg'
+  },
+
+  PATIOAURORA: {
+    name: 'Patio Aurora',
+    food: ['pizzas', 'empanadas', 'mojitos'],
+    address: 'Álvaro Obregón 126, Col. Roma, 06700 Ciudad de México, CDMX',
+    price: '$200-$400',
+    image: '../assets/images/aurora.jpg'
+  },
+
+  CANCINOCIBELES: {
+    name: 'Cancino Cibeles',
+    food: ['pizzas', 'pastas', 'empanadas', 'cerveza', 'ensaladas'],
+    address: 'Plaza Villa de Madrid 11, Col. Roma , 06700 Ciudad de México, CDMX',
+    price: '$150-$200',
+    image: '../assets/images/cancino.jpg'
+  },
+
+  PIZZANOSTRA: {
+    name: 'Pizza Nostra',
+    food: ['comida italiana', 'pastas', 'pizza', 'lasagna', 'pesto'],
+    address: 'Jalapa s/n, Col. Roma Sur, 06760 Ciudad de México, CDMX',
+    price: '$150-$200',
+    image: '../assets/images/nostra.jpg'
+  },
+
+  PIZZACLASICA: {
+    name: 'Pizza clásica',
+    food: ['pizzas', 'pastas', 'empanadas', 'cerveza', 'ensaladas'],
+    address: 'EJe 2 San Luis Potosí 60 A, Col. Roma, 06700 Ciudad de México, CDMX',
+    price: '$200-$250',
+    image: '../assets/images/clasica.jpg'
+  },
+
+  RICCHIEPIZZA: {
+    name: 'Ricchie Pizza',
+    food: ['pizzas', 'ensaladas', 'carnes frías'],
+    address: 'Tlaxcala 40, Col. Roma, 06700 Ciudad de México, CDMX',
+    price: '$150-$200',
+    image: '../assets/images/richie.jpg'
+  },
+
+  PIZZAPEPINO: {
+    name: 'Pizza Pepino',
+    food: ['comida italiana', 'pastas', 'pizza', 'lasagna', 'pesto'],
+    address: 'Morelia 90, Col. Roma, 06700 Ciudad de México, CDMX',
+    price: '$120-$300',
+    image: '../assets/images/pepino.jpg'
+  },
+
+  TRESCIELOS: {
+    name: 'Trescielos',
+    food: ['pizzas', 'pastas', 'empanadas', 'cerveza', 'ensaladas, helados'],
+    address: 'Manzanillo 45 Local B, Col. Roma, 06760 Ciudad de México, CDMX',
+    price: '$100-$300',
+    image: '../assets/images/trescielos.jpg'
+  }
+};
